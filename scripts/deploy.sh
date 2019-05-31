55c926bb5eda8141ccbe276b82396b4b6fb5aff4
Geoffrey Bauduin <bauduin.geo@gmail.com>
2019-05-31 21:03:48 +0200

    Fixing a bit more...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "export/" | grep -v index.json)
+files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "docs/champions" | grep -v index.json)
 for file in $files
 do
     echo "Processing $file"
-    raid-codex-cli champions page create --champion-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/champion.html
+    raid-codex-cli champions page create --champion-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/champion.html --data-directory $GOPATH/src/github.com/raid-codex/data
 done
 
 # raid-codex-cli website cache clear --cache-token $WP_CACHE_TOKEN
