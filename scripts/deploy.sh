35ff06098c35526326742f2c360a2ae72cdaaab7
Geoffrey Bauduin <bauduin.geo@gmail.com>
2019-05-31 21:16:27 +0200

    adding script to push skill files too

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,13 @@ do
     raid-codex-cli champions page create --champion-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/champion.html --data-directory $GOPATH/src/github.com/raid-codex/data
 done
 
+files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "docs/status-effects" | grep -v index.json)
+for file in $files
+do
+    echo "Processing $file"
+    raid-codex-cli status-effect page create --status-effect-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/status-effect.html --data-directory $GOPATH/src/github.com/raid-codex/data
+done
+
 # raid-codex-cli website cache clear --cache-token $WP_CACHE_TOKEN
 
 echo "Updated, don't forget to refresh cache"
