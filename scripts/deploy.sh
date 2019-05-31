1264eb60cf51072b109414a29313414da3e3d9d9
Geoffrey Bauduin <bauduin.geo@gmail.com>
2019-05-31 21:55:36 +0200

    More fixes (#3)
    * Fixing a bit more...
    
    * Fixing links to buffs/debuffs
    
    * adding script to push skill files too
    
    * Fixing issue with poisons at 2.5% not being correctly marked
    
    * Updated fortress goon to include skills, thanks to msbatx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "export/" | grep -v index.json)
+files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "docs/champions" | grep -v index.json)
 for file in $files
 do
     echo "Processing $file"
-    raid-codex-cli champions page create --champion-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/champion.html
+    raid-codex-cli champions page create --champion-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/champion.html --data-directory $GOPATH/src/github.com/raid-codex/data
+done
+
+files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "docs/status-effects" | grep -v index.json)
+for file in $files
+do
+    echo "Processing $file"
+    raid-codex-cli status-effect page create --status-effect-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/status-effect.html --data-directory $GOPATH/src/github.com/raid-codex/data
 done
 
 # raid-codex-cli website cache clear --cache-token $WP_CACHE_TOKEN
