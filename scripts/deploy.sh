067cf2733aa46d1979c058117ef03f848569553f
Geoffrey Bauduin <bauduin.geo@gmail.com>
2019-07-21 16:52:21 +0200

    Updating script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ files=$(git diff-tree --name-only --no-commit-id -r HEAD | grep "docs/status-eff
 for file in $files
 do
     echo "Processing $file"
-    raid-codex-cli status-effect page create --status-effect-file $file --template-file $GOPATH/src/github.com/raid-codex/tools/templates/status-effect.html --data-directory $GOPATH/src/github.com/raid-codex/data
+    raid-codex-cli status-effect page create --status-effect-file $file --template-folder $GOPATH/src/github.com/raid-codex/tools/templates/status-effect/ --data-directory $GOPATH/src/github.com/raid-codex/data
 done
 
 # raid-codex-cli website cache clear --cache-token $WP_CACHE_TOKEN
