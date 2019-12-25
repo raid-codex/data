8622c46b42fc2dd288e10b8383df633f602d5c22
Geoffrey Bauduin <bauduin.geo@gmail.com>
2019-12-25 21:41:35 +0100

    fix: skullcrown skill 2 and 4 were incomplete Fixes #67

--- scripts/refresh_champion.sh
+++ scripts/refresh_champion.sh
@@ -14,8 +14,5 @@ echo "Rebuilding champion json"
 raid-codex-cli champions sanitize --champion-file $GOPATH/src/github.com/raid-codex/data/docs/champions/current/$1.json --data-directory $GOPATH/src/github.com/raid-codex/data
 echo "Rebuilding champions index"
 bash $curdir/rebuild_index.sh champions
-echo "HTML page..."
-raid-codex-cli champions page generate --champion-file $GOPATH/src/github.com/raid-codex/data/docs/champions/current/$1.json --data-directory $GOPATH/src/github.com/raid-codex/data --template-folder "$GOPATH/src/github.com/raid-codex/tools/templates/champion" --output-file "$GOPATH/src/github.com/raid-codex/data/generated/champions/$1.html" --no-page
-echo "Small index..."
 python $curdir/shrink-index.py
 echo "OK"
