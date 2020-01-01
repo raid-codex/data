01b96238d950b421267a0eb9ce704abcd0595aab
Geoffrey Bauduin <bauduin.geo@gmail.com>
2020-01-01 17:51:49 +0100

    fix: added more schemas for validation

--- scripts/check_schema.sh
+++ scripts/check_schema.sh
@@ -5,7 +5,7 @@ exit_code=0
 function check()
 {
     # just keep stderr
-    raid-codex-cli schema validate --schema-file file://`pwd`/schemas/$1 --file file://`pwd`/$2 > /dev/null
+    raid-codex-cli schema validate --schema-file ./schemas/$1 --file file://`pwd`/$2 --definitions ./schemas/definitions.json  > /dev/null
     if [[ $? -ne 0 ]]
     then
         echo $2
@@ -23,8 +23,10 @@ function checkDirectory()
     check "${1}-index.json" "${2}/index.json"
 }
 
+checkDirectory fusion ./docs/fusions/current
 checkDirectory champion ./docs/champions/current
 checkDirectory status-effect ./docs/status-effects/current
+checkDirectory faction ./docs/factions/current
 
 
 exit $exit_code
