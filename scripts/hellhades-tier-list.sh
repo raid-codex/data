04cbf6f226c609646f20c175b48e34045b402468
Geoffrey Bauduin <bauduin.geo@gmail.com>
2020-12-06 12:40:10 +0100

    feat: updated hellhades tier list

--- scripts/hellhades-tier-list.sh
+++ scripts/hellhades-tier-list.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-wget "https://docs.google.com/spreadsheets/d/1YjETkvBMVKZr7CPDjL_iIy_Wa6-psHob6so6fgKLX7c/gviz/tq?tqx=out:csv&sheet=Legendaries" -O /tmp/legendaries.csv
-wget "https://docs.google.com/spreadsheets/d/1YjETkvBMVKZr7CPDjL_iIy_Wa6-psHob6so6fgKLX7c/gviz/tq?tqx=out:csv&sheet=Epics" -O /tmp/epics.csv
-wget "https://docs.google.com/spreadsheets/d/1YjETkvBMVKZr7CPDjL_iIy_Wa6-psHob6so6fgKLX7c/gviz/tq?tqx=out:csv&sheet=Rares" -O /tmp/rares.csv
-raid-codex-cli champions parse tier-list-hellhades --data-directory . --csv-file /tmp/legendaries.csv
-raid-codex-cli champions parse tier-list-hellhades --data-directory . --csv-file /tmp/epics.csv
-raid-codex-cli champions parse tier-list-hellhades --data-directory . --csv-file /tmp/rares.csv
+raid-codex-cli champions parse tier-list-hellhades --data-directory .
